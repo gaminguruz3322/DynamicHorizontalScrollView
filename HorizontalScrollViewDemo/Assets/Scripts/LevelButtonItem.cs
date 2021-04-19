889f7de10c262f87dc15cfc8f9bbaa4a620688b9
GaminGuruz <gaminguruz3322@gmail.com>
2021-04-19 13:39:00 +0530

    Final Project

---
+++ HorizontalScrollViewDemo/Assets/Scripts/LevelButtonItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// LevelButtonItem - attached to level button
+/// handle specific button related actions
+/// </summary>
+public class LevelButtonItem : MonoBehaviour
+{
+
+    [HideInInspector] public int levelIndex;
+    [HideInInspector] public LevelsScrollViewController levelsScrollViewController;
+    //
+    [SerializeField] Text levelButtonText;
+
+
+    private void Start()
+    {
+        levelButtonText.text =  (levelIndex + 1).ToString();
+    }
+
+    // click event of level button
+    public void OnLevelButtonClick()
+    {
+        levelsScrollViewController.OnLevelButtonClick(levelIndex);
+    }
+
+
+}
---
+++ HorizontalScrollViewDemo/Assets/Scripts/LevelsScrollViewController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// LevelsScrollViewController - generate scrollview items
+/// handle all things those required for scrollview controller
+/// </summary>
+public class LevelsScrollViewController : MonoBehaviour
+{
+
+    [SerializeField] Text levelNumberText;
+    [SerializeField] int numberOfLevels;
+    [SerializeField] GameObject levelBtnPref;
+    [SerializeField] Transform levelBtnParent;
+
+    private void Start()
+    {
+        LoadLevelButtons();
+    }
+
+    // load level buttons on game start
+    private void LoadLevelButtons()
+    {
+        for (int i = 0; i < numberOfLevels; i++)
+        {
+            GameObject levelBtnObj = Instantiate(levelBtnPref, levelBtnParent) as GameObject;
+            levelBtnObj.GetComponent<LevelButtonItem>().levelIndex = i;
+            levelBtnObj.GetComponent<LevelButtonItem>().levelsScrollViewController = this;
+        }
+    }
+
+    // user defined public method to handle something when user press any level button
+    // at present we are just changing level number, in future you can do anything that is required at here
+    public void OnLevelButtonClick(int levelIndex)
+    {
+        levelNumberText.text = "Level " + (levelIndex + 1);
+    }
+
+}
